a2fff604c893d34ae92f468d787e0fc987df5be6
Akash Ranjan Swain <aakashswain001@gmail.com>
2017-04-05 09:04:58 +0530

    owner Done
    bus owner part completed

---
+++ get_data_newbus.php
@@ -0,0 +1,68 @@
+<?php
+session_start();
+require_once 'dbconnect.php';
+
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+}
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+		
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
+$busname = $_POST['busname'];
+$from = $_POST['from'];
+$to = $_POST['to'];
+$price = $_POST['price'];
+echo $busname;
+$own_id=$_SESSION['user'];
+   $query = "INSERT INTO bus_details(owner_id,busname,frommm,tooo,price,locn,status) VALUES($own_id,'$busname','$from','$to','$price','$target_file','n')";
+   $res = mysql_query($query);
+    
+   if ($res) {
+      header("Location: ownerdash.php");
+	   exit;
+   } else {
+	   echo '<script type="text/javascript">alert("something error");</script>';
+   } 
+//mysql_connect(' localhost ' , root ,' ' );
+//mysql_select_db(' image_database ');
+//$insertquery = " insert into resize_images values('1,$out_image') ";
+//$result = mysql_query( $insertquery );
+
+?>
---
+++ owner_profile.php
@@ -0,0 +1,293 @@
+<?php
+session_start();
+include_once 'dbconnect.php';
+// it will never let you open index(login) page if session is set
+if ( isset( $_SESSION[ 'user' ] ) == "" ) {
+	header( "Location: index.php" );
+	exit;
+}
+$e = $_SESSION['user'];
+$res = mysql_query( "SELECT fname,email,servicename,adhar,addr,phone FROM bus_owners WHERE id= $e" );
+		$row = mysql_fetch_array( $res );
+			$count = mysql_num_rows( $res );
+			if ( $count = 1 ) {
+					
+					$a1=$row['servicename'];
+					$a2 = $row['adhar'];
+					$a3 = $row['addr'];
+					$a4 = $row['phone'];
+					$a5 = $row['email'];
+					$a6 = $row['fname'];
+					
+			} else {
+			echo 'error';
+				}
+//for password change
+ if ( isset( $_POST[ 'update' ] ) ){
+ 	// clean user inputs to prevent sql injections
+ 	$pass = trim( $_POST[ 'changepass' ] );
+	$password= md5($pass);
+	 
+	 $ex = $_SESSION[ 'user' ];
+  	$query = "UPDATE bus_owners
+SET password = '$password'
+WHERE id = $ex";
+ 	$res = mysql_query( $query );
+ 	if ( $res ) {
+ 		echo '<script type="text/javascript">alert("Password Successfully changed");</script>';
+
+ 	} else {
+
+ 		echo '<script type="text/javascript">alert("some error");</script>';
+ 	}
+
+ }
+//to check no of bus
+
+	$bus_count = 0;
+		$sql = "SELECT id FROM bus_details WHERE owner_id='$e'";
+		if ( $result = mysqli_query( $con1, $sql ) ) {
+			// Fetch one and one row
+			while ( $row1 = mysqli_fetch_row( $result ) ) {
+			$bus_count++;
+			
+			}
+			// Free result set
+			mysqli_free_result( $result );
+		}
+
+?>
+
+
+
+<!DOCTYPE html>
+<!--[if IE 9]> <html lang="en" class="ie9"> <![endif]-->
+<!--[if !IE]><!-->
+<html lang="en">
+<!--<![endif]-->
+	<head>
+		<meta charset="utf-8">
+		<title>Buskaro | One stop bus booking website</title>
+		<meta name="description" content="Worthy a Bootstrap-based, Responsive HTML5 Template">
+		<meta name="author" content="htmlcoder.me">
+
+		<!-- Mobile Meta -->
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+		<!-- Favicon -->
+		<link rel="shortcut icon" href="images/favicon.ico">
+
+		<!-- Web Fonts -->
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,400,700,300&amp;subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Raleway:700,400,300' rel='stylesheet' type='text/css'>
+
+		<!-- Bootstrap core CSS -->
+		<link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+
+		<!-- Font Awesome CSS -->
+		<link href="fonts/font-awesome/css/font-awesome.css" rel="stylesheet">
+
+		<!-- Plugins -->
+		<link href="css/animations.css" rel="stylesheet">
+
+		<!-- Worthy core CSS file -->
+		<link href="css/style.css" rel="stylesheet">
+
+		<!-- Custom css --> 
+		<link href="css/custom.css" rel="stylesheet">
+
+		<link href="css/myStyles.css" rel="stylesheet">
+
+<style>
+  html{height:100%;}
+
+ 
+
+
+.box {
+	width: 400px;
+	height: 60px;
+	-moz-border-radius: 30px;
+	-webkit-border-radius: 30px;
+	border-radius: 30px;
+	-moz-background-clip: padding;
+	-webkit-background-clip: padding-box;
+	background-clip: padding-box;
+	background-image: -moz-linear-gradient(90deg, #dfdfdf 0%, #e9e9e9 100%);
+	background-image: -o-linear-gradient(90deg, #dfdfdf 0%, #e9e9e9 100%);
+	background-image: -webkit-linear-gradient(90deg, #dfdfdf 0%, #e9e9e9 100%);
+	background-image: linear-gradient(90deg, #dfdfdf 0%, #e9e9e9 100%);
+	position: relative;
+	top: 120px;
+	display: table-cell;
+	vertical-align: middle;
+	text-align: center;
+}
+
+p {
+  color: #999;
+  display: inline;
+  position: absolute;
+  bottom: 13px;
+  left: 20px;
+  margin-left: 150px;
+  margin-top: 40px;
+  padding-top: 50px;
+}
+.user {
+  color: #000;
+
+}
+#btm{
+	border:2px solid;
+	border-radius: 25px;
+
+}
+</style>
+
+		<script>
+			$('#myModal').modal('show');
+		</script>
+
+	</head>
+
+	<body class="no-trans">
+		<!-- scrollToTop -->
+		<!-- ================ -->
+		<div class="scrollToTop"><i class="icon-up-open-big"></i></div>
+
+<?php include 'include/headertype4.php'; ?>
+
+		<center>
+  <div class="box" style="margin-top: 200px;">
+    <p >Hi, <span class="user"><?= $a6;?></span></p>
+  </div>
+  
+</center>
+
+<center>
+
+	<div class="container">
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xs-offset-0 col-sm-offset-0 col-md-offset-3 col-lg-offset-3 toppad" style="margin-top: 150px;">
+   
+   
+          <div class="panel panel-info">
+          <br>
+            
+            <div class="panel-body" >
+              <div class="row">
+                <div class="col-md-3 col-lg-3 " align="center">  </div>
+                
+         
+                <div class=" col-md-9 col-lg-9 " > 
+                
+                  <table class="table table-user-information" >
+                    <tbody>
+                    
+                      <tr>
+       <td>Bus Service Name: </td>
+                         <td><?= $a1;?></td>
+                        
+                      </tr>
+                      <tr>
+                      	<td>Adhar Num: </td>
+                         <td><?= $a2;?></td>
+                      </tr>
+                      <tr>
+                        <td>Address: </td>
+                        <td><?= $a3;?></td>
+                        </tr>
+                       <tr>
+                        <td>Email Id: </td>
+                        <td><?= $a5;?></td>
+                        
+                      </tr>
+                        <td>Phone Number:</td>
+                        <td><?= $a4;?></td>
+                        </tr>
+                        </tr>
+                        <td>No of Buses: </td>
+                        <td><?= $bus_count;?></td>
+                        </tr>
+                        <tr>
+                        	<td><button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal" id="btm">Change Password</button></td>
+                        </tr>
+                     
+                     
+                    </tbody>
+                  </table>
+                  
+                  </div>
+              </div>
+            </div>
+            </div>
+            </div>
+            </div>
+            </div>
+        </center>
+        
+        <?php  include 'include/subfooter.php' ?>
+        
+        
+        <div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Change Password</h4>
+      </div>
+      <div class="modal-body">
+        <div class="col-sm-4">
+        	Reset Password:        	
+        </div>
+     <form method="post">   <div class="col-sm-8">
+        	<input type="Password" name="changepass">
+        </div>
+        <br>
+        <br>
+        <div class="col-sm-4">
+        	Confirm Password:        	
+        </div>
+        <div class="col-sm-8">
+        	<input type="Password" name="confipass">
+        </div>
+      </div>
+      <br>
+      <br>
+      <div class="modal-footer">
+       <input type="submit" value="updade" class="btn btn-info btn-round" id="registerButtonService" name="update">
+        <button type="button" class="btn btn-danger" data-dismiss="modal" id="btm">Close</button>
+</form> 
+      </div>
+    </div>
+
+  </div>
+</div>
+		<!-- JavaScript files placed at the end of the document so the pages load faster
+		================================================== -->
+		<!-- Jquery and Bootstap core js files -->
+		<script type="text/javascript" src="plugins/jquery.min.js"></script>
+		<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+
+		<!-- Modernizr javascript -->
+		<script type="text/javascript" src="plugins/modernizr.js"></script>
+
+		<!-- Isotope javascript -->
+		<script type="text/javascript" src="plugins/isotope/isotope.pkgd.min.js"></script>
+		
+		<!-- Backstretch javascript -->
+		<script type="text/javascript" src="plugins/jquery.backstretch.min.js"></script>
+
+		<!-- Appear javascript -->
+		<script type="text/javascript" src="plugins/jquery.appear.js"></script>
+
+		<!-- Initialization of Plugins -->
+		<script type="text/javascript" src="js/template.js"></script>
+
+		<!-- Custom Scripts -->
+		<script type="text/javascript" src="js/custom.js"></script>
+
+	</body>
+</html>
