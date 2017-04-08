5cf6807b84ad73874a727d8093f85b2fe0094921
Sanghamitra1234 <sanghota4567@gmail.com>
2017-04-08 13:43:23 -0400

    admin pages
    php done

---
+++ admin/a_login.php
@@ -0,0 +1,42 @@
+
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="file:///C|/Users/lenovo/Documents/GitHub/busKaro/website/styleb1.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<title>Admin register</title>
+</head>
+
+<body>
+
+<div class="container" style="padding-top: 10px;">
+<div class="row">
+	<div class="col-md-8">
+	<form class="form-horizontal" method="post" role="form">
+		<div class="form-group" >
+			<label for="from" class="control-label col-sm-2">Username :</label>
+			<div class="col-sm-3 col-lg-6">
+				<input type="text" id="from" name="from" class="form-control"> 
+			</div>
+		</div>
+		
+		<div class="form-group">
+			<label for="d_on" class="control-label col-sm-2">Password:</label>
+			<div class="col-sm-3 col-lg-6">
+				<input type="password" id="d_on" name="d_on" class="form-control">
+		</div>
+	<input type="submit" name="user"> 		</div>
+
+		</form>
+	</div>
+	
+   </div>
+	</div>
+</body>
+</html>
